d43f5b2b878f7069590ea029e6cdb12b8db25950
Thomas <thomasm0077@gmail.com>
2023-11-28 14:07:25 +0530

    Update  - ui change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_ripple_animation/simple_ripple_animation.dart';
 
 void main() {
@@ -22,38 +25,150 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Thomas M Kurialassery"),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 100)),
-            RippleAnimation(
-              color: Colors.deepOrange,
-              delay: Duration(milliseconds: 100),
-              repeat: true,
-              minRadius: 75,
-              ripplesCount: 6,
-              duration: Duration(milliseconds: 16 * 300),
-              child: CircleAvatar(
-                minRadius: 75,
-                maxRadius: 75,
-                backgroundImage: NetworkImage(
-                    "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"),
-              ),
-            )
-          ],
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: const Text("Thomas M Kurialassery"),
+      // ),
+      // drawer: const NavigationDrawer(children:[
+      //   Text("Item1"),
+      //   Text("Item2"),
+      //   Text("Item3"),
+      //   Text("Item4"),
+      //   Text("Item5"),
+      // ]),
+      body: FooterView(
+        footer: Footer(
+          padding: const EdgeInsets.all(5.0),
+          backgroundColor: const Color(0x0DFFFFFF),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.only(top: 20)),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      SizedBox(
+                          height: 45.0,
+                          width: 45.0,
+                          child: Center(
+                            child: Card(
+                              elevation: 5.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    25.0), // half of height and width of Image
+                              ),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.audiotrack,
+                                  size: 20.0,
+                                ),
+                                color: Color(0xFF162A49),
+                                onPressed: () {},
+                              ),
+                            ),
+                          )),
+                      Container(
+                          height: 45.0,
+                          width: 45.0,
+                          child: Center(
+                            child: Card(
+                              elevation: 5.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    25.0), // half of height and width of Image
+                              ),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.fingerprint,
+                                  size: 20.0,
+                                ),
+                                color: Color(0xFF162A49),
+                                onPressed: () {},
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                          height: 45.0,
+                          width: 45.0,
+                          child: Center(
+                            child: Card(
+                              elevation: 5.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    25.0), // half of height and width of Image
+                              ),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.call,
+                                  size: 20.0,
+                                ),
+                                color: const Color(0xFF162A49),
+                                onPressed: () {},
+                              ),
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 20)),
+                const Text(
+                  'Copyright ©2020, All Rights Reserved.',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12.0,
+                      color: Color(0xFF162A49)),
+                ),
+                const Text(
+                  'Powered by Nexsport',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12.0,
+                      color: Color(0xFF162A49)),
+                ),
+              ]),
         ),
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.only(top: 100)),
+                const RippleAnimation(
+                  color: Color(0xFFC8CEC7),
+                  delay: Duration(milliseconds: 100),
+                  repeat: true,
+                  minRadius: 75,
+                  ripplesCount: 5,
+                  duration: Duration(milliseconds: 16 * 300),
+                  child: CircleAvatar(
+                    minRadius: 75,
+                    maxRadius: 75,
+                    backgroundImage: AssetImage("assets/me.png"),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 80)),
+                Text(
+                  "Thomas M Kurialassery",
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2)),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
